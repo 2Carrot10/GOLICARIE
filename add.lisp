1fcc4d84b13d97d292ad175bf13ac340c9db7102
JJ-IsShort <crazygamer.js73@gmail.com>
2026-04-28 14:55:36 -0400

    progress

--- add.lisp
+++ add.lisp
@@ -2,59 +2,49 @@
   ; Doing what I probably should've done before and yoinking examples from the Mackie paper
   ; `r` generally means `ret` for `return`
   (spiders (
-    (Z 
-      >r
-    )
-    (S
-      ; This is directly connecting to any functions that may touch it
-      >r
-      ; And is "protecting" the number behind it.
-      -n
-    )
-    (P
-      >arg1
-      -arg2
-      -r
-    )
-    (T
-      >arg1
-      -arg2
-      -r
-    )
-  ))
+            (Z 
+              >r)
+    
+            (S
+              ; This is directly connecting to any functions that may touch it
+              >r
+              ; And is "protecting" the number behind it.
+              -n)
+    
+            (P
+              >arg1
+              -arg2
+              -r)
+    
+            (T
+              >arg1
+              -arg2
+              -r)))
+    
+  
   (rules (
-      (
-        ; The fact that this won't react when on arg2 is actually perfectly fine. 
-        ; The goal is to just move everything to the other side
-        (Z.r P.arg1)
-        ; Because adding to zero is a no-op, we just connect the return to the second argument
-        (P.arg1 P.arg2)
-      )
-      (
-        ; Move from the left to the right
-        (S.r P$0.arg1)
-        (
-          ; We aren't actually "moving things around". The nodes in the active pair are deleted, but we have a reference to their edges.
-          S.r P$0.r
-          S.n P$1.r
-          P$1.arg1 S.n
-          P$1.arg2 P$0.arg2
-        )
-      )
-    )
-  )
+          ( ; The fact that this won't react when on arg2 is actually perfectly fine. 
+            ; The goal is to just move everything to the other side
+            (Z.r P.arg1)
+            ; Because adding to zero is a no-op, we just connect the return to the second argument
+            (P.arg1 P.arg2))
+          ( ; Move from the left to the right
+            (S.r P$0.arg1)
+            (
+              ; We aren't actually "moving things around". The nodes in the active pair are deleted, but we have a reference to their edges.
+              P$0.r S.r
+              S.n P$1.r
+              P$1.arg1 S.n
+              P$0.arg2 P$1.arg2))))
   (graph
     ; Starting off simple. Adding 1 and 2 together. 
     (
       ; Successor of zero
-      S$0.n Z.r
+      S$0.n Z$0.r
       ; Is first argument to the addition
       S$0.r P$0.arg1
       ; Successor of the successor of zero
-      S$1.n Z.r
+      S$1.n Z$1.r
       S$1.r S$2.n
       ; Is second argument to the addition
-      S$2.r P$0.arg2
-    )
-  )
-)
+      S$2.r P$0.arg2)))
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 // deno --watch-hmr --allow-all main.ts
 import MultiGraph from "https://esm.sh/graphology";
-import {Parser} from "./parser.ts";
+import { Parser } from "./parser.ts";
 
 // Parse args
 let args = Deno.args;
@@ -12,12 +12,12 @@ if (args.length == 0) {
 
 let code = Deno.readTextFileSync(args[0]);
 
-let nested = new Parser(code).parse(); 
+let nested = new Parser(code).parse();
 
 function parse_wire(e) {
   const [x, port] = e.split(".");
   const [clss, name = "default"] = x.split("$");
-  return {clss, name, port}
+  return { clss, name, port };
 }
 
 const Constructors = {
@@ -41,41 +41,207 @@ const Constructors = {
   // We are largely just using graphology for this. So all we do here is convert from the instructions into functions that graphology can understand.
   graph: class {
     graph: MultiGraph;
-    constructor(args: string[]) {
-      this.graph = new MultiGraph({type: "mixed"});
+    book: { node: string; port: string }[][];
+    constructor(args: string[], spider_defs: spiders[]) {
+      this.graph = new MultiGraph({ type: "mixed", multi: true });
 
       let x = args.map(parse_wire);
-      console.log(x);
-      let prev_id = null;
-      let prev_port = null;
+      const vars = new Set(x.map((el) => `${el.clss}$${el.name}`));
+      const glmap = new Map(
+        Array.from(vars, (el) => [
+          el,
+          `${crypto.randomUUID().slice(0, 8)}___${el}`,
+        ]),
+      );
+      vars.forEach((el) => {
+        this.graph.addNode(glmap.get(el), {
+          clss: el.split("$")[0],
+          name: el.split("$")[1],
+        });
+      });
+
       // Now we can add each edge to the graphology graph. We can ignore the fact that we are adding nodes that already exist, as graphology will just ignore that.
       // The way I'm doing it is a little gross, but to prevent having to explicitly declare variables, we just kinda make nodes when we see that wires refer to them
-      for (let [i, {clss, name, port}] of x.entries()) {
-        const id = `${crypto.randomUUID().slice(0, 8)}___${clss}$${name}`;
+      let prev_id = null;
+      let prev_port = null;
+      for (let [i, { clss, name, port }] of x.entries()) {
+        const local = `${clss}$${name}`;
+        const id = glmap.get(local); // Get the global id of the local node
         if (i % 2 == 0) {
           prev_id = id;
           prev_port = port;
-          this.graph.addNode(id, {clss, name, port});
         } else {
-          this.graph.addNode(id, {clss, name, port});
           // We add `addDirectedEdgeWithKey` based on the agent.
-          this.graph.addUndirectedEdgeWithKey(`${id}_${port}<>${prev_id}_${prev_port}`, prev_id, id);
+          this.graph.addUndirectedEdgeWithKey(
+            `${id}.${port}<>${prev_id}.${prev_port}`,
+            prev_id, // Goes between the global ids of the nodes
+            id,
+            {
+              left_id: id,
+              right_id: prev_id,
+              left_port: port,
+              right_port: prev_port,
+            },
+          );
         }
       }
-      
+
+      const spider_ntp = new Map(
+        Array.from(spider_defs, (el) => [
+          el.name,
+          new Map(
+            Array.from(el.ports, (port) => [port.slice(1), port.slice(0, 1)]),
+          ),
+        ]),
+      );
       // Comes in pairs, so we chunk as such
       let edges = [];
       for (let i = 0; i < x.length; i += 2) {
         edges.push([x[i], x[i + 1]]);
       }
+      this.book = edges
+        .map((el) => {
+          const left_def = spider_ntp.get(el[0].clss);
+          const right_def = spider_ntp.get(el[1].clss);
+          const left_dir = left_def?.get(el[0].port);
+          const right_dir = right_def?.get(el[1].port);
+          return { dir: `${left_dir}${right_dir}`, edges: el };
+        })
+        .filter((el) => el.dir === ">>")
+        .map((el) => {
+          // Convert from local node definitions to global for the reduction to be able to use it
+          return [
+            {
+              node: glmap.get(`${el.edges[0].clss}$${el.edges[0].name}`),
+              port: el.edges[0].port,
+            },
+            {
+              node: glmap.get(`${el.edges[1].clss}$${el.edges[1].name}`),
+              port: el.edges[1].port,
+            },
+          ];
+        });
+    }
+  },
+};
 
+function apply_rewrite(
+  graph: graph,
+  rule: rules,
+  nodes: string[],
+  active_pair: string,
+) {
+  var external_nodes = new Map();
+  for (const left_edge of graph.graph.edges(nodes[0])) {
+    if (left_edge !== active_pair) {
+      const outgoing_attrs = graph.graph.getEdgeAttributes(left_edge);
+      external_nodes.set(
+        `${rule.inputs[0].clss}$${rule.inputs[0].name}.${outgoing_attrs.left_port}`,
+        left_edge.split(".")[0],
+      );
+    }
+  }
+  for (const right_edge of graph.graph.edges(nodes[1])) {
+    if (right_edge !== active_pair) {
+      const outgoing_attrs = graph.graph.getEdgeAttributes(right_edge);
+      external_nodes.set(
+        `${rule.inputs[1].clss}$${rule.inputs[1].name}.${outgoing_attrs.left_port}`,
+        right_edge.split(".")[0],
+      );
+    }
+  }
+  graph.graph.dropNode(nodes[0]);
+  graph.graph.dropNode(nodes[1]);
+  console.log(external_nodes);
+  const vars = new Set(rule.outputs.map((el) => `${el.clss}$${el.name}`));
+  const glmap = new Map(
+    Array.from(vars, (el) => [
+      el,
+      `${crypto.randomUUID().slice(0, 8)}___${el}`,
+    ]),
+  );
+  console.log(rule);
+  console.log(glmap);
+  vars.forEach((el) => {
+    graph.graph.addNode(glmap.get(el), {
+      clss: el.split("$")[0],
+      name: el.split("$")[1],
+    });
+  });
+
+  let prev_id = null;
+  let prev_port = null;
+  for (let [i, { clss, name, port }] of rule.outputs.entries()) {
+    const local = `${clss}$${name}`;
+    const id = glmap.get(local); // Get the global id of the local node
+    if (i % 2 == 0) {
+      if (external_nodes.has(`${local}.${port}`)) {
+        // Introduces constraint that when connecting to external nodes, they must be the first argument TODO: fix
+        prev_id = external_nodes.get(`${local}.${port}`);
+      } else {
+        prev_id = id;
+      }
+      prev_port = port;
+    } else {
+      // We add `addDirectedEdgeWithKey` based on the agent.
+      graph.graph.addUndirectedEdgeWithKey(
+        `${id}.${port}<>${prev_id}.${prev_port}`,
+        prev_id, // Goes between the global ids of the nodes
+        id,
+        {
+          left_id: id,
+          right_id: prev_id,
+          left_port: port,
+          right_port: prev_port,
+        },
+      );
     }
   }
 }
-// Realized that the API of the nesting actually matched quite well to how `new Map(xss)` works
 
+function reduce(graph: Constructors.graph, rules: Constructors.rules) {
+  const PopFromBook = () => {
+    const idx = Math.floor(Math.random() * graph.book.length);
+    const edge = graph.book.pop(idx);
+    return edge;
+  };
+
+  while (graph.book.length > 0) {
+    const target_redex: { node: string; port: string }[] = PopFromBook();
+    var edge_id = `${target_redex[0].node}.${target_redex[0].port}<>${target_redex[1].node}.${target_redex[1].port}`;
+    if (!graph.graph.hasEdge(edge_id)) {
+      const other_possible_edge_id = `${target_redex[1].node}.${target_redex[1].port}<>${target_redex[0].node}.${target_redex[0].port}`;
+      if (graph.graph.hasEdge(other_possible_edge_id)) {
+        edge_id = other_possible_edge_id;
+      } else {
+        throw new Error(
+          "Unexpected error: Redex being looked up does not exist!",
+        );
+      }
+    }
+    const edge_attributes = graph.graph.getEdgeAttributes(edge_id);
+    const left_id = edge_attributes.left_id;
+    const right_id = edge_attributes.right_id;
+    const left_node_attributes = graph.graph.getNodeAttributes(left_id);
+    const right_node_attributes = graph.graph.getNodeAttributes(right_id);
+    console.log(target_redex);
+    const rule_to_apply = rules.find((el) => {
+      return (
+        left_node_attributes.clss === el.inputs[1].clss && // Don't ask me why these need to be flipped
+        edge_attributes.left_port === el.inputs[1].port &&
+        right_node_attributes.clss === el.inputs[0].clss &&
+        edge_attributes.right_port === el.inputs[0].port
+      );
+    });
+    apply_rewrite(graph, rule_to_apply, [left_id, right_id], edge_id);
+  }
+}
+
+// Realized that the API of the nesting actually matched quite well to how `new Map(xss)` works
 const prog = new Map(nested);
 const spiders = prog.get("spiders").map((e) => new Constructors.spiders(...e));
 const rules = prog.get("rules").map((e) => new Constructors.rules(...e));
-const graph = new Constructors.graph(prog.get("graph"));
+const graph = new Constructors.graph(prog.get("graph"), spiders);
+console.log(graph);
+reduce(graph, rules);
 console.log(graph);
